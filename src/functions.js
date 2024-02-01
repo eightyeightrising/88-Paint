6acd9a9b1f2c70468b893df81338485bacd9f52d
Anh Dao <daoanh440@gmail.com>
2024-02-01 12:02:50 +1100

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -3570,7 +3570,12 @@ function read_image_file(blob, callback) {
 			}
 			file_format = "image/png";
 			//HERE
-			const image_data = new ImageData(new Uint8ClampedArray(rgba), width, height);
+			const imageWidth = width > window.innerWidth ? window.innerWidth : width;
+			const imageHeight = height > window.innerHeight ? window.innerHeight : height;
+
+			console.log(imageWidth, imageHeight)
+
+			const image_data = new ImageData(new Uint8ClampedArray(rgba), imageWidth, imageHeight);
 			callback(null, { file_format, monochrome, palette, image_data, source_blob: blob });
 		} else if (detected_type_id === "tiff_be" || detected_type_id === "tiff_le") {
 			// IFDs = image file directories
